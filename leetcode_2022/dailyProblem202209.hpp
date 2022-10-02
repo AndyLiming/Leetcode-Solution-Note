868765d9aed2779832b700649d9f8a85a3d1b7ea
AndyLiming <mingli6250@163.com>
2022-10-02 16:58:16 +0800

    complete all problems in 2022.09

--- leetcode_2022/dailyProblem202209.hpp
+++ leetcode_2022/dailyProblem202209.hpp
@@ -4,6 +4,54 @@
 
 class daily202209 {
 public:
+  // 2022.09.01 - No 1475
+  vector<int> finalPrices(vector<int>& prices) {
+    int n = prices.size();
+    vector<int> res;
+    for (int i = 0; i < n; ++i) {
+      res.push_back(prices[i]);
+      for (int j = i + 1; j < n; ++j) {
+        if (prices[j] <= prices[i]) {
+          res[i] -= prices[j];
+          break;
+        }
+      }
+    }
+    return res;
+  }
+  // 2022.09.02 - No 687
+  int res = 0;
+  int longestUnivaluePath(TreeNode* root) {
+    res = 0;
+    if (root == nullptr) return res;
+    int ans = rec(root);
+    //cout<<ans<<endl;
+    return res;
+  }
+
+  int rec(TreeNode* root) {
+    if ((!root->left) && (!root->right)) return 0;
+    int left = 0, right = 0;
+    if (root->left) left = rec(root->left);
+    if (root->right) right = rec(root->right);
+    int ans = 0;
+    if ((root->left && root->val == root->left->val) && (root->right && root->val == root->right->val)) {
+      res = max(res, left + right + 2);
+      ans = max(left, right) + 1;
+    }
+    else if (root->left && (root->val == root->left->val)) {
+      res = max(res, left + 1);
+      ans = left + 1;
+    }
+    else if (root->right && (root->val == root->right->val)) {
+      res = max(res, right + 1);
+      ans = right + 1;
+    }
+    else {
+      ans = 0;
+    }
+    return ans;
+  }
 // 2022.09.03 - No 646
   int findLongestChain(vector<vector<int>>& pairs) {
     if (pairs.empty()) return 0;
@@ -52,7 +100,28 @@ public:
     }
     return ans;
   }
+  //2022.09.05 - No 652
+  unordered_map<string, TreeNode*> seen;
+  unordered_set<TreeNode*> repeat;
+  vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+    dfs(root);
+    return { repeat.begin(), repeat.end() };
+  }
 
+  string dfs(TreeNode* node) {
+    if (!node) {
+      return "";
+    }
+    string serial = to_string(node->val) + "(" + dfs(node->left) + ")(" + dfs(node->right) + ")";
+    auto it = seen.find(serial); 
+    if (it != seen.end()) {
+      repeat.insert(it->second);
+    }
+    else {
+      seen[serial] = node;
+    }
+    return serial;
+  }
   //2022.09.06 - No 828
   int uniqueLetterString(string s) {
     vector<vector<int>>ids(26, vector<int>());
@@ -257,9 +326,62 @@ public:
     return seen.size();
   }
   //2022.09.16 - No 850
-  //int rectangleArea(vector<vector<int>>& rectangles) {
+  int rectangleArea(vector<vector<int>>& rectangles) {
+    int n = rectangles.size();
+    vector<int> hbound;
+    for (const auto& rect : rectangles) {
+      // 下边界
+      hbound.push_back(rect[1]);
+      // 上边界
+      hbound.push_back(rect[3]);
+    }
+    sort(hbound.begin(), hbound.end());
+    hbound.erase(unique(hbound.begin(), hbound.end()), hbound.end());
+    int m = hbound.size();
+    // 「思路与算法部分」的 length 数组并不需要显式地存储下来
+    // length[i] 可以通过 hbound[i+1] - hbound[i] 得到
+    vector<int> seg(m - 1);
 
-  //}
+    vector<tuple<int, int, int>> sweep;
+    for (int i = 0; i < n; ++i) {
+      // 左边界
+      sweep.emplace_back(rectangles[i][0], i, 1);
+      // 右边界
+      sweep.emplace_back(rectangles[i][2], i, -1);
+    }
+    sort(sweep.begin(), sweep.end());
+
+    long long ans = 0;
+    for (int i = 0; i < sweep.size(); ++i) {
+      int j = i;
+      while (j + 1 < sweep.size() && get<0>(sweep[i]) == get<0>(sweep[j + 1])) {
+        ++j;
+      }
+      if (j + 1 == sweep.size()) {
+        break;
+      }
+      // 一次性地处理掉一批横坐标相同的左右边界
+      for (int k = i; k <= j; ++k) {
+        auto idx=get<1>(sweep[k]);
+        auto diff = get<2>(sweep[k]);
+        int left = rectangles[idx][1], right = rectangles[idx][3];
+        for (int x = 0; x < m - 1; ++x) {
+          if (left <= hbound[x] && hbound[x + 1] <= right) {
+            seg[x] += diff;
+          }
+        }
+      }
+      int cover = 0;
+      for (int k = 0; k < m - 1; ++k) {
+        if (seg[k] > 0) {
+          cover += (hbound[k + 1] - hbound[k]);
+        }
+      }
+      ans += static_cast<long long>(cover)* (get<0>(sweep[j + 1]) - get<0>(sweep[j]));
+      i = j;
+    }
+    return ans % static_cast<int>(1e9 + 7);
+  }
   //2022.09.17 - No 1624
   int maxLengthBetweenEqualCharacters(string s) {
     vector<int>first(26, -1),last(26,-1);
@@ -276,7 +398,56 @@ public:
     }
     return ans;
   }
-  //2022.09.18 - No
+  //2022.09.18 - No 827
+  const vector<int> d = { 0, -1, 0, 1, 0 };
+  bool valid(int n, int x, int y) {
+    return x >= 0 && x < n && y >= 0 && y < n;
+  }
+
+  int dfs(const vector<vector<int>>& grid, int x, int y, vector<vector<int>>& tag, int t) {
+    int n = grid.size(), res = 1;
+    tag[x][y] = t;
+    for (int i = 0; i < 4; i++) {
+      int x1 = x + d[i], y1 = y + d[i + 1];
+      if (valid(n, x1, y1) && grid[x1][y1] == 1 && tag[x1][y1] == 0) {
+        res += dfs(grid, x1, y1, tag, t);
+      }
+    }
+    return res;
+  }
+
+  int largestIsland(vector<vector<int>>& grid) {
+    int n = grid.size(), res = 0;
+    vector<vector<int>> tag(n, vector<int>(n));
+    unordered_map<int, int> area;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (grid[i][j] == 1 && tag[i][j] == 0) {
+          int t = i * n + j + 1;
+          area[t] = dfs(grid, i, j, tag, t);
+          res = max(res, area[t]);
+        }
+      }
+    }
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (grid[i][j] == 0) {
+          int z = 1;
+          unordered_set<int> connected;
+          for (int k = 0; k < 4; k++) {
+            int x = i + d[k], y = j + d[k + 1];
+            if (!valid(n, x, y) || tag[x][y] == 0 || connected.count(tag[x][y]) > 0) {
+              continue;
+            }
+            z += area[tag[x][y]];
+            connected.insert(tag[x][y]);
+          }
+          res = max(res, z);
+        }
+      }
+    }
+    return res;
+  }
   //2022.09.19 - No 1636
   vector<int> frequencySort(vector<int>& nums) {
     unordered_map<int, int> tab;
@@ -321,7 +492,38 @@ public:
     vector<bool>dp(1 << n, true);
     return dfs_698(start, 0, n, dp, nums, avg);
   }
-  //2022.09.21 - No xxx
+  //2022.09.21 - No 854
+  int kSimilarity(string s1, string s2) {
+    int n = s1.size();
+    queue<pair<string, int>> qu;
+    unordered_set<string> visit;
+    qu.emplace(s1, 0);
+    visit.emplace(s1);
+    for (int step = 0;; step++) {
+      int sz = qu.size();
+      for (int i = 0; i < sz; i++) {
+        auto cur = qu.front().first;
+        auto pos = qu.front().second;
+        qu.pop();
+        if (cur == s2) {
+          return step;
+        }
+        while (pos < n && cur[pos] == s2[pos]) {
+          pos++;
+        }
+        for (int j = pos + 1; j < n; j++) {
+          if (cur[j] != s2[j] && cur[j] == s2[pos]) { // 剪枝，只在 cur[j] != s2[j] 时去交换
+            swap(cur[pos], cur[j]);
+            if (!visit.count(cur)) {
+              visit.emplace(cur);
+              qu.emplace(cur, pos + 1);
+            }
+            swap(cur[pos], cur[j]);
+          }
+        }
+      }
+    }
+  }
   //2022.09.22 - No 1640
   bool canFormArray(vector<int>& arr, vector<vector<int>>& pieces) {
     unordered_map<int,int> tab;
@@ -339,7 +541,66 @@ public:
     }
     return true;
   }
-  //2022.9.24 - No 1652
+  //2022.09.23 - No 707
+  class MyLinkedList {
+  public:
+    MyLinkedList() {
+      this->size = 0;
+      this->head = new ListNode(0);
+    }
+
+    int get(int index) {
+      if (index < 0 || index >= size) {
+        return -1;
+      }
+      ListNode* cur = head;
+      for (int i = 0; i <= index; i++) {
+        cur = cur->next;
+      }
+      return cur->val;
+    }
+
+    void addAtHead(int val) {
+      addAtIndex(0, val);
+    }
+
+    void addAtTail(int val) {
+      addAtIndex(size, val);
+    }
+
+    void addAtIndex(int index, int val) {
+      if (index > size) {
+        return;
+      }
+      index = max(0, index);
+      size++;
+      ListNode* pred = head;
+      for (int i = 0; i < index; i++) {
+        pred = pred->next;
+      }
+      ListNode* toAdd = new ListNode(val);
+      toAdd->next = pred->next;
+      pred->next = toAdd;
+    }
+
+    void deleteAtIndex(int index) {
+      if (index < 0 || index >= size) {
+        return;
+      }
+      size--;
+      ListNode* pred = head;
+      for (int i = 0; i < index; i++) {
+        pred = pred->next;
+      }
+      ListNode* p = pred->next;
+      pred->next = pred->next->next;
+      delete p;
+    }
+  private:
+    int size;
+    ListNode* head;
+  };
+  //2022.09.24 - No 1652
   vector<int> decrypt(vector<int>& code, int k) {
     int n = code.size();
     vector<int>ans;
@@ -363,7 +624,7 @@ public:
     }
     return ans;
   }
-  //2022.9.25 - No 788
+  //2022.09.25 - No 788
   // 学一下数位DP
   int rotatedDigits(int n) {
     int cnt = 0;
@@ -380,7 +641,7 @@ public:
     }
     return cnt;
   }
-  //2022.9.26 - No mianshi 17.19
+  //2022.09.26 - No mianshi 17.19
   vector<int> missingTwo(vector<int>& nums) {
     int xorS = 0;
     for (auto num : nums) xorS ^= num;
@@ -405,7 +666,7 @@ public:
     }
     return { n1, n2 };
   }
-  //2022.9.27 - No mianshi 01.02
+  //2022.09.27 - No mianshi 01.02
   bool CheckPermutation(string s1, string s2) {
     unordered_map<char, int>tab;
     for (auto c : s1) {
@@ -417,7 +678,7 @@ public:
     }
     return true;
   }
-  //2022.9.28 - No mianshi 01.02
+  //2022.09.28 - No mianshi 01.02
   int getKthMagicNumber(int k) {
     vector<int>res;
     res.push_back(1);
@@ -431,12 +692,12 @@ public:
     }
     return res.back();
   }
-  //2022.9.29 - No mianshi 01.09
+  //2022.09.29 - No mianshi 01.09
   bool isFlipedString(string s1, string s2) {
     string s1s1 = s1 + s1;
     return (s1.size() == s2.size() && s1s1.find(s2) != string::npos);
   }
-  //2022.9.30 - No mianshi 01.08
+  //2022.09.30 - No mianshi 01.08
   void setZeroes(vector<vector<int>>& matrix) {
     int n = matrix.size(), m = matrix[0].size();
     unordered_set<int>rows, cols;
