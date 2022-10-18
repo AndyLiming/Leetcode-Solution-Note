fb9f308575de12835a0ba197f2c0950bb590f957
AndyLiming <mingli6250@163.com>
2022-10-18 14:51:51 +0800

    update No 72 in top100

--- leetcode_2022/lcTop100.hpp
+++ leetcode_2022/lcTop100.hpp
@@ -170,6 +170,23 @@ public:
   //No 70
     //No 71
   //No 72
+  int minDistance(string word1, string word2) {
+    int n = word1.size(), m = word2.size();
+    vector<vector<int>>dp(n + 1, vector<int>(m + 1));
+    for (int i = 0; i <= n; ++i)dp[i][0] = i;
+    for (int j = 0; j <= m; ++j)dp[0][j] = j;
+    for (int i = 1; i <= n; ++i) {
+      for (int j = 1; j <= m; ++j) {
+        if (word1[i-1] == word2[j-1]) {
+          dp[i][j] = min(min(dp[i - 1][j] + 1, dp[i][j - 1]+1), dp[i - 1][j - 1]);
+        }
+        else {
+          dp[i][j] = min(min(dp[i - 1][j] + 1, dp[i][j - 1]+1), dp[i - 1][j - 1]+1);
+        }
+      }
+    }
+    return dp[n][m];
+  }
   //No 73
   //No 74
   //No 75
