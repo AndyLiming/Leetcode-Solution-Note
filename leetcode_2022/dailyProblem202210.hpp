6f59837a030a25441d3dcc03ded0d77a8ee3fbce
AndyLiming <mingli6250@163.com>
2022-10-25 15:30:59 +0800

    update daily problem 10.22, 10.24, 10.25

--- leetcode_2022/dailyProblem202210.hpp
+++ leetcode_2022/dailyProblem202210.hpp
@@ -386,4 +386,110 @@ public:
     }
     return ans;
   }
+  //2022.10.22 - No 1235
+  int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+    vector<vector<int>>jobs;
+    int n = startTime.size();
+    for (int i = 0; i < n; ++i) {
+      jobs.push_back({ startTime[i],endTime[i],profit[i] });
+    }
+    sort(jobs.begin(), jobs.end(), [](const vector<int> & v1, const vector<int> & v2) {return (v1[1] < v2[1]); });
+    vector<int>dp(n + 1, 0);
+    for (int i = 1; i <= n; ++i) {
+      int stt = jobs[i - 1][0];
+      int left = 0, right = i - 1;
+      while (left < right) {
+        int mid = (left + right+1) / 2;
+        if (jobs[mid][1] > stt) right = mid - 1;
+        else {
+          left = mid;
+        }
+      }
+      if (left == 0 && jobs[left][1] > stt) left = -1;
+      dp[i] = max(dp[i - 1], dp[left+1] + jobs[i - 1][2]);
+    }
+    return dp[n];
+  }
+  //2022.10.24 - No 915
+  int partitionDisjoint(vector<int>& nums) {
+    int n = nums.size(), ans = 0;
+    vector<int>minR(n, 0);
+    minR[n - 1] = nums[n - 1];
+    for (int i = n - 2; i >= 0; --i) {
+      minR[i] = min(minR[i + 1], nums[i]);
+    }
+    int maxL = 0;
+    for (int i = 0; i < n - 1; ++i) {
+      maxL = max(maxL, nums[i]);
+      if (maxL <= minR[i + 1]) return i + 1;
+    }
+    return 0;
+  }
+  int partitionDisjoint_2(vector<int>& nums) {
+    int n = nums.size();
+    int leftMax = nums[0], leftPos = 0, curMax = nums[0];
+    for (int i = 1; i < n - 1; i++) {
+      curMax = max(curMax, nums[i]);
+      if (nums[i] < leftMax) {
+        leftMax = curMax;
+        leftPos = i;
+      }
+    }
+    return leftPos + 1;
+  }
+  //2022.10.25 - No 934
+  int shortestBridge(vector<vector<int>>& grid) {
+    int n = grid.size();
+    vector<vector<int>> dirs = { {-1, 0}, {1, 0}, {0, 1}, {0, -1} };
+    vector<pair<int, int>> island;
+    queue<pair<int, int>> qu;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (grid[i][j] == 1) {
+          qu.emplace(i, j);
+          grid[i][j] = -1;
+          while (!qu.empty()) {
+            auto [x, y] = qu.front();
+            qu.pop();
+            island.emplace_back(x, y);
+            for (int k = 0; k < 4; k++) {
+              int nx = x + dirs[k][0];
+              int ny = y + dirs[k][1];
+              if (nx >= 0 && ny >= 0 && nx < n && ny < n && grid[nx][ny] == 1) {
+                qu.emplace(nx, ny);
+                grid[nx][ny] = -1;
+              }
+            }
+          }
+          for (auto&& [x, y] : island) {
+            qu.emplace(x, y);
+          }
+          int step = 0;
+          while (!qu.empty()) {
+            int sz = qu.size();
+            for (int i = 0; i < sz; i++) {
+              auto [x, y] = qu.front();
+              qu.pop();
+              for (int k = 0; k < 4; k++) {
+                int nx = x + dirs[k][0];
+                int ny = y + dirs[k][1];
+                if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
+                  if (grid[nx][ny] == 0) {
+                    qu.emplace(nx, ny);
+                    grid[nx][ny] = -1;
+                  }
+                  else if (grid[nx][ny] == 1) {
+                    return step;
+                  }
+                }
+              }
+            }
+            step++;
+          }
+        }
+      }
+    }
+    return 0;
+  }
 };
